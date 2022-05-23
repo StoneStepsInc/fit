d26e84e50e159fd7eea1d091fd6cc2553324aa37
Stone Steps <support@stonesteps.ca>
2022-05-23 16:39:42 -0400

    Added a Bash script to download SHA-256 source

--- get-sha256.bat
+++ get-sha256.bat
@@ -1,17 +1,21 @@
 @echo off
 
+setlocal
+
 if EXIST src\sha256 goto :haveit
+
+set GIT_COMMIT=28c561fa25b62dc9fce075c1af21ee2579cf1d59
 
 rem
 rem This library is not versioned, so get the source at
 rem the commit where it was tested with this project.
 rem
-curl -L -o sha256.zip https://github.com/ilvn/SHA256/archive/28c561fa25b62dc9fce075c1af21ee2579cf1d59.zip
+curl -L -o sha256.zip https://github.com/ilvn/SHA256/archive/%GIT_COMMIT%.zip
 
 call 7z x sha256.zip
 del /Q sha256.zip
 
-move SHA256-28c561fa25b62dc9fce075c1af21ee2579cf1d59 src\sha256
+move SHA256-%GIT_COMMIT% src\sha256
 cd src\sha256
 
 rem
