9d3d083684742a3fd7fa6bdc6ff4b04110da7140
Stone Steps <support@stonesteps.ca>
2022-12-10 11:56:11 -0500

    Fixed incorrect to/from versions in the upgrade script

--- sql/upgrade-db_3.0-4.0.sql
+++ sql/upgrade-db_3.0-4.0.sql
@@ -1,8 +1,8 @@
 --
--- This script upgrades the database schema from version 2.0 to
--- version 3.0.
+-- This script upgrades the database schema from version 3.0 to
+-- version 4.0.
 --
--- sqlite3 sqlite.db < upgrade-db_2.0-3.0.sql
+-- sqlite3 sqlite.db < upgrade-db_3.0-4.0.sql
 --
 -- Version literals are not used because .param does not work in
 -- PRAGMA. Search for VER_FROM and VER_TO comments to identify
@@ -30,12 +30,13 @@ CREATE TEMPORARY TABLE user_version_trap (
 -- the value in WHEN is the expected version
 INSERT INTO user_version_trap VALUES (
   CASE (select user_version from pragma_user_version())
-    WHEN 20 THEN 1                  -- VER_FROM
+    WHEN 30 THEN 1                          -- VER_FROM
     ELSE NULL
   END
 );
 
-.print Upgrading database to version 3.0
+--                                             VER_TO
+.print Upgrading database to version 4.0
 
 CREATE TABLE IF NOT EXISTS upgrades (
   upgrade_from INTEGER NOT NULL PRIMARY KEY,
@@ -52,17 +53,17 @@ INSERT INTO upgrades (
   upgrade_time
 ) VALUES (
   (select user_version from pragma_user_version()),
-  30,                               -- VER_TO
+  40,                                       -- VER_TO
   CAST(strftime('%s', 'now') AS INTEGER)
 );
 
-DROP INDEX ix_scansets_scan_files;
+DROP INDEX ix_scansets_scan_file;
 
 CREATE UNIQUE INDEX ix_scansets_version_scan ON scansets (version_id, scan_id);
 
 --
 -- Set the target database version
 --
-PRAGMA user_version=30;             -- VER_TO
+PRAGMA user_version=40;                     -- VER_TO
 
 COMMIT TRANSACTION;
