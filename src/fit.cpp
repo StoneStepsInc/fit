966328b30cf99c480533fc77b820c8ca56be807d
Stone Steps <support@stonesteps.ca>
2022-12-10 10:47:58 -0500

    Updated version to 2.0.2

--- src/fit.cpp
+++ src/fit.cpp
@@ -46,7 +46,7 @@ using namespace std::literals::string_literals;
 namespace fit {
 
 static const char *title = "File Integrity Tracker";
-static const char *version = "2.0.1+" STR_BUILD_NUMBER(BUILD_NUMBER);
+static const char *version = "2.0.2+" STR_BUILD_NUMBER(BUILD_NUMBER);
 static const char *copyright = "Copyright (c) 2022 Stone Steps Inc.";
 
 //
