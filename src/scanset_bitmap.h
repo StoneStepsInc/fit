f2a9ce29bb88e9d1a0ab396079cb2939ea896470
Stone Steps <support@stonesteps.ca>
2025-11-30 18:16:12 -0500

    Added a few comments in scanset_bitmap_t (no code changes)

--- src/scanset_bitmap.h
+++ src/scanset_bitmap.h
@@ -7,21 +7,43 @@
 
 namespace fit {
 
+//
+// Provides a container for tracking scanset identifiers, SQLite
+// rowid values, for each file version included into the scanset
+// that is being represented by a single bit.
+// 
+// When the bitmap is created, all of the bits are set. During
+// a verification scan, bits file version bits are cleared for
+// each scanned file that was found in the database. After the
+// verification scan is complete, the bits that are remaining
+// identify the files that existed at the time of the base
+// scan and are not present in the verification scan (i.e.
+// either removed or cannot be accessed anymore).
+// 
+// Multiple scanset bitmaps are expected to be created for the
+// same scanset, processed by different threads, and then merged,
+// via the `scanset_bitmap_t::update` call, into a single scanset
+// bitmap that can be used for reporting removed files.
+//
 class scanset_bitmap_t {
    public:
+      //
+      // A scanset bitmap iterator that returns rowid values
+      // corresponding to non-zero bits in the scanset bitmap.
+      //
       class const_iterator {
          private:
             const std::vector<uint64_t>& scanset_bitmap;
 
-            uint64_t first_rowid;
+            uint64_t first_rowid;      // a copy of scanset_bitmap_t::first_rowid
 
-            uint64_t last_rowid;
+            uint64_t last_rowid;       // a copy of scanset_bitmap_t::last_rowid
 
-            size_t elem_offset;
+            size_t elem_offset;        // a zero-based offset of the current uint64_t element
 
-            size_t bit_offset;
+            size_t bit_offset;         // a zero-based bit offset from the most significant bit at elem_offset (i.e. 64th bit is zero)
 
-            uint64_t bit_mask;
+            uint64_t bit_mask;         // a bit mask for the bit identified by bit_offset
 
          public:
             explicit const_iterator(uint64_t first_rowid, uint64_t last_rowid, const std::vector<uint64_t>& scanset_bitmap, uint64_t current_rowid);
