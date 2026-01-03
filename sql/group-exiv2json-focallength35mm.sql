5289abc5edf01557f946ac1c72a70912c3817f9d
Stone Steps <support@stonesteps.ca>
2026-01-03 15:55:12 -0500

    Changed SQL to show 35mm focal length in multiples of 5

--- sql/group-exiv2json-focallength35mm.sql
+++ sql/group-exiv2json-focallength35mm.sql
@@ -2,20 +2,21 @@
 -- sqlite3 -box -cmd ".param set @SCAN_ID N" sqlite.db < sql/group-exiv2json-focallength35mm.sql
 --
 -- Groups images by $.Exif.Photo.FocalLengthIn35mmFilm field in
--- Exiv2Json, rounded up/down to the nearest multiple of 10.
+-- Exiv2Json, rounded up/down to the nearest multiple of 5.
 -- If @SCAN_ID is omitted, the last scan ID is used.
 --
 SELECT
-    COUNT(*),
-    cast(round(json_extract(Exiv2Json, '$.Exif.Photo.FocalLengthIn35mmFilm')/10., 0)*10 as INTEGER) as FocalLengthIn35mm
+    CAST(round(json_extract(Exiv2Json, '$.Exif.Photo.FocalLengthIn35mmFilm')/5., 0)*5 AS INTEGER) AS FocalLengthIn35mm,
+    COUNT(*)
 FROM 
     scansets
     JOIN versions ON version_id = versions.rowid 
     JOIN files ON file_id = files.rowid 
     JOIN exif ON exif_id = exif.rowid
 WHERE
-    scan_id = coalesce(@SCAN_ID, (SELECT MAX(rowid) FROM scans), 0)
+    scan_id = coalesce(@SCAN_ID, (SELECT MAX(rowid) FROM scans), 0) AND
+    FocalLengthIn35mm IS NOT NULL
 GROUP BY
-    cast(round(json_extract(Exiv2Json, '$.Exif.Photo.FocalLengthIn35mmFilm')/10., 0)*10 as INTEGER)
+    CAST(round(json_extract(Exiv2Json, '$.Exif.Photo.FocalLengthIn35mmFilm')/5., 0)*5 AS INTEGER)
 ORDER BY
-    1 DESC
+    2 DESC
